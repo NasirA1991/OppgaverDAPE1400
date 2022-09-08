262ed6f675779f336de79f842ac4edc1a952bb68
Nasir A <evilleader91@gmail.com>
2022-09-08 06:14:06 +0200

    Initial commit

--- src/oppgavevariabel2/OppgaveVariabel2.java
+++ src/oppgavevariabel2/OppgaveVariabel2.java
@@ -9,9 +9,20 @@ public class OppgaveVariabel2 {
         String innData2 = showInputDialog("Skriv inn et desimaltall");
         String innData3 = showInputDialog("Skriv inn et desimaltall");
 
-        double tall1 = Double.parseDouble(innData1);
-        double tall2 = Double.parseDouble(innData2);
-        double tall3 = Double.parseDouble(innData3);
+        double tall1;
+        double tall2;
+        double tall3;
+        try{
+                tall1 = Double.parseDouble(innData1);
+                tall2 = Double.parseDouble(innData2);
+                tall3 = Double.parseDouble(innData3);
+        }
+        catch (Exception e){
+                tall1=0;
+                tall2=0;
+                tall3=0;
+                System.out.println(e);
+        }
         double gjsnitt = (tall1+tall2+tall3)/3;
         String svar = "Gjennomsnittet av tallene "+tall1+", "+tall2+" og "+tall3+" er "+gjsnitt;
 
