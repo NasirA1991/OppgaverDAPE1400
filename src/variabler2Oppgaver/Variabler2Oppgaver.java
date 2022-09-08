c80d85f5fd23c7555abdc8c5bb4b525ce0b1df6d
Nasir <evilleader91@gmail.com>
2022-09-08 04:17:55 +0200

    Initial commit

---
+++ src/variabler2Oppgaver/Variabler2Oppgaver.java
@@ -0,0 +1,21 @@
+package variabler2Oppgaver;
+
+import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class Variabler2Oppgaver {
+    public static void main(String [] args){
+        String innFornavn = showInputDialog("Skriv inn fornavnet ditt");
+        String innEtternavn = showInputDialog("Skriv inn etternavnet ditt");
+        String innAlder = showInputDialog("Hvor gammel er du?");
+        String innAdresse = showInputDialog("Hva er adressen din?");
+        String innPoststed = showInputDialog("Hva er posttedet ditt?");
+        String innPostnr = showInputDialog("Hva er postnr ditt?");
+        String ut = "Navn: "+innFornavn+" "+innEtternavn+"\n"+"Adresse: "+innAdresse+"\n"+"Postnummer: "+innPostnr+"\n"+"Poststed: "+innPoststed+"\n"+"Alder: "+innAlder+" år";
+
+        System.out.println(ut);
+        //showMessageDialog(null,ut);
+    }
+}
+
+}
