9c1cda4ef4a996a877f72d189f605522df12bdf7
Nasir A <evilleader91@gmail.com>
2022-09-08 05:09:36 +0200

    Initial commit

--- src/variablerDesimalTall/VariablerDesimalTall.java
+++ src/variablerDesimalTall/VariablerDesimalTall.java
@@ -8,7 +8,7 @@ public class VariablerDesimalTall {
         String innStreng1 = showInputDialog("Skriv inn et desimaltall");
         String innStreng2 = showInputDialog("Skriv inn et desimaltall til");
         double tall1 = Double.parseDouble(innStreng1);
-        double tall2 = Double.parseDouble(innStreng2);
+        double tall2 = Double.parseDouble(innStreng2);   //NB!!! Skal du dividere HELTALL (integer) må en eller begge av dem være definert som desimaltall (double), hvis ikke får du alltid et heltall som svar tilbake!!!
         double divisjon = tall1/tall2;
         System.out.println(divisjon);
         //showMessageDialog(null, "Divisjonen ble "+divisjon);
