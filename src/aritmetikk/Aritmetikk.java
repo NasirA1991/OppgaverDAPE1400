e31a1b533b025c19c80250142d28f1a80547ee45
Nasir A <evilleader91@gmail.com>
2022-09-13 06:35:15 +0200

    Initial commit

---
+++ src/aritmetikk/Aritmetikk.java
@@ -0,0 +1,14 @@
+package aritmetikk;
+
+public class Aritmetikk {
+    public static void main(String [] args){
+
+//kan omgjøre heltall/heltall til desimaltall ved resultat "caste"
+        int tall1 = 5;
+        int tall2 = 10;
+        double resultat;
+        resultat = (double) tall1 / (double) tall2;
+
+        System.out.println(resultat);
+    }
+}
