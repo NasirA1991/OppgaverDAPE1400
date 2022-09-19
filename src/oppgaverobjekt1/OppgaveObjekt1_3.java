a0d687459378b36f7c4b79d2462a3d8a863b1da7
Nasir A <evilleader91@gmail.com>
2022-09-19 20:12:56 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/variabler2Oppgave/oppgaverobjekt1/Bøker.class
    #	out/production/variabler2Oppgave/oppgaverobjekt1/OppgaveObjekt1_1.class

--- src/oppgaverobjekt1/OppgaveObjekt1_3.java
+++ src/oppgaverobjekt1/OppgaveObjekt1_3.java
@@ -1,38 +1,42 @@
 package oppgaverobjekt1;
 
-import static javax.swing.JOptionPane.showInputDialog;
-import static javax.swing.JOptionPane.showMessageDialog;
+import static javax.swing.JOptionPane.*;
 
 class Person {
 
     String navn, adresse, telefonnr;
     int fødselsår, alder;
 
+
     public void regneUtAlder() {
         alder = 2022 - fødselsår;
     }
 
     public void utSkriftAlt() {
-        System.out.print(navn+" med adresse "+adresse+" med telefonnummer "+telefonnr+" er "+alder+" år ");
+        String utData = "Navn: "+navn+"\n"+"Adresse: "+adresse+"\n"+"Telefonnummer: "+telefonnr+"\n"+"Alder: "+alder;
+        showMessageDialog(null, utData);
     }
 
-
-
-
-
-
 }
-
 
 public class OppgaveObjekt1_3 {
     public static void main(String [] args ){
         Person p1 = new Person();
-        p1.navn = "Nasir Amin";
-        p1.adresse = "Nordkappgata 13";
-        p1.telefonnr = "22099115";
-        p1.fødselsår = 1991;
+        p1.navn = showInputDialog("Hva heter du?");
+        p1.adresse = showInputDialog("Hva er adressen din?");
+        p1.telefonnr = showInputDialog("Hva er telefonnummeret ditt?");
+        String innFÅr = showInputDialog("Skriv inn fødselsnummeret ditt");
+        p1.fødselsår = Integer.parseInt(innFÅr);
         p1.regneUtAlder();
         p1.utSkriftAlt();
+
+        /*Person p2 = new Person();
+        p2.navn = "Shoaib Amin";
+        p2.adresse = "Nordkappgata 13";
+        p2.telefonnr = "40469602";
+        p2.fødselsår = 1992;
+        p2.regneUtAlder();
+        p2.utSkriftAlt();*/
 
 
     }
