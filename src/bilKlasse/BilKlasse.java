8ccd08d5dc185753d4107946bb34d3830510b375
Nasir A <evilleader91@gmail.com>
2022-09-19 07:30:53 +0200

    Initial commit

---
+++ src/bilKlasse/BilKlasse.java
@@ -0,0 +1,17 @@
+package bilKlasse;
+
+class Bil {
+    public String farge;
+
+    public void visFarge(){
+    System.out.print("Fargen til bilen er "+farge);
+    }
+}
+
+public class BilKlasse {
+    public static void main (String [] args){
+        Bil nyBil = new Bil();
+        nyBil.farge = "blå";
+        nyBil.visFarge();
+    }
+}
---
+++ src/bilKlasse/objects91/Objects91.java
@@ -0,0 +1,21 @@
+package bilKlasse.objects91;
+
+class Bil {
+    private String farge;
+
+    public void settFarge (String fargen){
+        farge = fargen;
+    }
+    public String hentFarge(){
+        return farge;
+    }
+}
+
+public class Objects91 {
+    public static void main(String [] args){
+        Bil nyBil = new Bil();
+        nyBil.settFarge("blå");
+        String bilFargen = nyBil.hentFarge();
+        System.out.print("Fargen til bilen er "+bilFargen);
+    }
+}
